c81ae6b5e2049ce720a83530d7ffa5241df71f42
Hamza-HM <mohzilla8@gmail.com>
2023-11-11 23:11:29 +0100

    optimised views

--- DougaTech_Back/users/api/views.py
+++ DougaTech_Back/users/api/views.py
@@ -1,15 +1,15 @@
 from django.contrib.auth import get_user_model
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, UpdateModelMixin, DestroyModelMixin, CreateModelMixin
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer, UserProfileSerializer, AddressSerializer
-from ..models import UserProfile
+from ..models import UserProfile, Address
 
 User = get_user_model()
 
-class UserControleViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, DestroyModelMixin, GenericViewSet):
+class UserControleViewSet(ListModelMixin, UpdateModelMixin, DestroyModelMixin, GenericViewSet):
     serializer_class = UserProfileSerializer
     queryset = UserProfile.objects.all()
     permission_classes = [IsAdminUser]
@@ -22,17 +22,15 @@ class UserProfileViewSet(RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     def get_object(self):
         user = self.request.user
         return user.profile
+    
+class AddressControlViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, DestroyModelMixin, GenericViewSet):
+    serializer_class = AddressSerializer
+    permission_classes = [IsAdminUser]
+    queryset = Address.objects.all()
 
-    def retrieve(self, request,*args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data, status.HTTP_200_OK)
-
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        if not serializer.is_valid():
-            return Response({'error': serializer.errors}, status.HTTP_400_BAD_REQUEST)
-        self.perform_update(serializer)
-        return Response(serializer.data, status.HTTP_200_OK)
+class AddressUserViewset(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericViewSet):
+    serializer_class = AddressSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_object(self):
+        return self.request.user.Address
